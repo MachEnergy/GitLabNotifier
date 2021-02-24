ba2865e97960bf65fe7edc9080d33a200e6cb7f6
Ryan McGhee <mcghee.ryan@gmail.com>
2021-02-23 19:01:39 -0500

    Add project files.

---
+++ FeedParser.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GitLabNotifier
+{
+  /// <summary>
+  /// Represents a feed item.
+  /// </summary>
+  public class Item
+  {
+    public string Link { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime PublishDate { get; set; }
+    public FeedType FeedType { get; set; }
+
+    public Item()
+    {
+      Link = "";
+      Title = "";
+      Content = "";
+      PublishDate = DateTime.Today;
+      FeedType = FeedType.Atom;
+    }
+  }
+
+  /// <summary>
+  /// A simple RSS, RDF and ATOM feed parser.
+  /// </summary>
+  static public class FeedParser
+  {
+    /// <summary>
+    /// Parses an Atom feed and returns a <see cref="IList&amp;lt;Item&amp;gt;"/>.
+    /// </summary>
+    static public IList<Item> ParseAtom(string url, [Optional] ref IList<string> errors)
+    {
+      try
+      {
+        XDocument doc = XDocument.Load(url);
+        // Feed/Entry
+
+        var test_entries = doc.Root.Elements().Where(i => i.Name.LocalName == "entry");
+        List<Item> test_entry_list = new List<Item>();
+
+        foreach (var test_entry in test_entries)
+        {
+          Item newItem = new Item();
+
+          newItem.FeedType = FeedType.Atom;
+
+          try
+          {
+            newItem.Content = test_entry.Elements().First(i => i.Name.LocalName == "content")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Link = test_entry.Elements().First(i => i.Name.LocalName == "link").Attribute("href")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.PublishDate = ParseDate(test_entry.Elements().First(i => i.Name.LocalName == "published")?.Value);
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Title = test_entry.Elements().First(i => i.Name.LocalName == "title")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          test_entry_list.Add(newItem);
+        }
+
+        return test_entry_list;
+      }
+      catch (Exception ex)
+      {
+        errors.Append(ex.Message);
+        return new List<Item>();
+      }
+    }
+
+    /// <summary>
+    /// Parses an RSS feed and returns a <see cref="IList&amp;lt;Item&amp;gt;"/>.
+    /// </summary>
+    static public IList<Item> ParseRss(string url, [Optional] ref IList<string> errors)
+    {
+      try
+      {
+        XDocument doc = XDocument.Load(url);
+        // RSS/Channel/item
+
+        var test_channels = doc.Root.Descendants().Where(i => i.Name.LocalName == "channel");
+        var test_items = test_channels.Descendants().Where(i => i.Name.LocalName == "item");
+        List<Item> test_item_list = new List<Item>();
+
+        foreach (var test_item in test_items)
+        {
+          Item newItem = new Item();
+
+          newItem.FeedType = FeedType.RSS;
+
+          try
+          {
+            newItem.Content = test_item.Elements().First(i => i.Name.LocalName == "description")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Link = test_item.Elements().First(i => i.Name.LocalName == "link")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.PublishDate = ParseDate(test_item.Elements().First(i => i.Name.LocalName == "pubDate")?.Value);
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Title = test_item.Elements().First(i => i.Name.LocalName == "title")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          test_item_list.Add(newItem);
+        }
+
+        return test_item_list;
+      }
+      catch (Exception ex)
+      {
+        errors.Append(ex.Message);
+        return new List<Item>();
+      }
+    }
+
+    /// <summary>
+    /// Parses an RDF feed and returns a <see cref="IList&amp;lt;Item&amp;gt;"/>.
+    /// </summary>
+    static public IList<Item> ParseRdf(string url, [Optional] ref IList<string>errors)
+    {
+      try
+      {
+        XDocument doc = XDocument.Load(url);
+        // <item> is under the root
+
+        var test_items = doc.Root.Descendants().Where(i => i.Name.LocalName == "item");
+        List<Item> test_item_list = new List<Item>();
+
+        foreach (var test_item in test_items)
+        {
+          Item newItem = new Item();
+
+          newItem.FeedType = FeedType.RDF;
+
+          try
+          {
+            newItem.Content = test_item.Elements().First(i => i.Name.LocalName == "description")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Link = test_item.Elements().First(i => i.Name.LocalName == "link")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.PublishDate = ParseDate(test_item.Elements().First(i => i.Name.LocalName == "date")?.Value);
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          try
+          {
+            newItem.Title = test_item.Elements().First(i => i.Name.LocalName == "title")?.Value;
+          }
+          catch (Exception ex)
+          {
+            errors.Append(ex.Message);
+          }
+
+          test_item_list.Add(newItem);
+        }        
+
+        return test_item_list;
+      }
+      catch (Exception ex)
+      {
+        errors.Append(ex.Message);
+        return new List<Item>();
+      }
+    }
+
+    static private DateTime ParseDate(string date)
+    {
+      DateTime result;
+      if (DateTime.TryParse(date, out result))
+        return result;
+      else
+        return DateTime.MinValue;
+    }
+  }
+  /// <summary>
+  /// Represents the XML format of a feed.
+  /// </summary>
+  public enum FeedType
+  {
+    /// <summary>
+    /// Really Simple Syndication format.
+    /// </summary>
+    RSS,
+    /// <summary>
+    /// RDF site summary format.
+    /// </summary>
+    RDF,
+    /// <summary>
+    /// Atom Syndication format.
+    /// </summary>
+    Atom
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitLabNotifier
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window, INotifyPropertyChanged
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      foreach (FeedType ftype in Enum.GetValues(typeof(FeedType)))
+      {
+        ComboBoxFeedTypes.Items.Add(ftype);
+      }
+
+      m_Settings.Reload();
+
+      if (string.IsNullOrWhiteSpace(m_Settings.UserFeedType))
+      {
+        m_Settings.UserFeedType = m_Settings.DefaultFeedType;
+      }
+
+      if (string.IsNullOrWhiteSpace(m_Settings.UserUrl))
+      {
+        m_Settings.UserUrl = m_Settings.DefaultUrl;
+      }
+
+      m_Settings.Save();
+
+      CurrentFeedType = (FeedType)Enum.Parse(typeof(FeedType), m_Settings.UserFeedType);
+      CurrentUrl = m_Settings.UserUrl;
+    }
+
+    AppSettings m_Settings = new AppSettings();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public FeedType CurrentFeedType
+    {
+      get { return m_CurrentFeedType; }
+      set { m_CurrentFeedType = value; OnPropertyChanged(); }
+    }
+    private FeedType m_CurrentFeedType;
+
+    public string CurrentUrl
+    {
+      get { return m_CurrentUrl; }
+      set { m_CurrentUrl = value; OnPropertyChanged(); }
+    }
+    private string m_CurrentUrl;
+
+    private void ButtonParse_Click(object sender, RoutedEventArgs e)
+    {
+      ParseFeed();
+    }
+
+    private void ParseFeed()
+    {
+      IList<string> errors = new List<string>();
+
+      switch (CurrentFeedType)
+      {
+        case FeedType.RSS:
+          ListViewFeed.ItemsSource = FeedParser.ParseRss(TextBoxUrl.Text, ref errors);
+          break;
+        case FeedType.RDF:
+          ListViewFeed.ItemsSource = FeedParser.ParseRdf(TextBoxUrl.Text, ref errors);
+          break;
+        case FeedType.Atom:
+          ListViewFeed.ItemsSource = FeedParser.ParseAtom(TextBoxUrl.Text, ref errors);
+          break;
+      }
+
+      if (errors.Count > 0)
+      {
+        MessageBox.Show(errors.ToString(), $"({errors.Count}) Errors During Parse");
+      }
+    }
+
+    private void Window_Closing(object sender, CancelEventArgs e)
+    {
+      m_Settings.UserFeedType = CurrentFeedType.ToString();
+      m_Settings.UserUrl = CurrentUrl;
+      m_Settings.Save();
+    }
+
+    private void ItemsPresenter_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      string url = ((Item)((Grid)sender).DataContext).Link;
+      Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+    }
+  }
+}
